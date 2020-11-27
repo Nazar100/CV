b7edff4567875d561e17bee3437e9f449da31e02
Nazar Leshchyshyn <lesh1.nazar@gmail.com>
2020-11-27 21:49:05 +0100

    js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./css/common.css";
 import "./css/styles.scss";
 import "./js/type";
 import "./js/io";
-// import "./js/timeouts";
+import "./js/timeouts";
 import "./js/timer";
 import "./js/scroll";
 import "./js/skills";
